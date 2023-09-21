96e77a9ebbdcfb2a24b7ef48b2cfad16b804a511
Travis Lange <travislange12@gmail.com>
2023-09-21 10:43:25 -0400

    added example project

--- broken-example/src/lib.rs
+++ broken-example/src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use gdnative::api::{CSGBox, CSGSphere, Node};
+use gdnative::prelude::*;
+
+#[derive(NativeClass, Default)]
+#[inherit(Node)]
+pub struct FallingBall {
+    ball: Option<Ref<CSGSphere>>,
+    ground: Option<Ref<CSGBox>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[methods]
+impl FallingBall {
+    fn new(_base: &Node) -> Self {
+        FallingBall::default()
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[method]
+    fn _ready(&mut self, #[base] _base: &Node) {
+        self.ball = Some(
+            unsafe { _base.get_node_as::<CSGSphere>("ball") }
+                .unwrap()
+                .claim(),
+        );
+        self.ground = Some(
+            unsafe { _base.get_node_as::<CSGBox>("ground") }
+                .unwrap()
+                .claim(),
+        );
+
+        let ball = unsafe { self.ball.unwrap().assume_safe() };
+        let ball_pos = ball.translation();
     }
 }
